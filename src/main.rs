cbee2dd16b292d5ff2ceb01fd947aacbab20ee40
ferbass <ferbass@gmail.com>
2020-09-28 22:12:22 +0900

    triggering a page fault

--- src/main.rs
+++ src/main.rs
@@ -21,13 +21,15 @@ pub extern "C" fn _start() -> ! {
 
     gat_os::init();
 
-    x86_64::instructions::interrupts::int3();
+    // trigger a page fault
+    unsafe {
+        *(0xdeadbeef as *mut u64) = 42;
+    }
 
     #[cfg(test)]
     test_main();
 
     println!("It did not crash!");
-
     loop {}
 }
 
