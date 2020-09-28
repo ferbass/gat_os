bcc692885a96cde24be6fc5c0460e5131e015a6a
ferbass <ferbass@gmail.com>
2020-09-28 23:05:45 +0900

    handle stack overflow

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,12 +1,16 @@
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 use crate::println;
+use crate::gdt;
 use lazy_static::lazy_static;
 
 lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
         idt.breakpoint.set_handler_fn(breakpoint_handler);
-        idt.double_fault.set_handler_fn(double_fault_handler);
+        unsafe {
+            idt.double_fault.set_handler_fn(double_fault_handler)
+                .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
+        }
         idt
     };
 }
--- src/main.rs
+++ src/main.rs
@@ -21,10 +21,11 @@ pub extern "C" fn _start() -> ! {
 
     gat_os::init();
 
-    // trigger a page fault
-    unsafe {
-        *(0xdeadbeef as *mut u64) = 42;
+    fn stack_overflow() {
+        stack_overflow();
     }
+
+    stack_overflow();
 
     #[cfg(test)]
     test_main();
